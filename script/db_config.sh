c6db6c536d2b5c0cb1daef5eb6ddb77eed8c52e3
tanmally <tm@beyondbytes.co.in>
2018-05-24 02:30:19 +0530

    Added mongo db recipe

---
+++ script/db_config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+MONGODB_SKIP_AUTH_SETUP={{SKIP_AUTH_SETUP}}
+
+if [ ! "$MONGODB_SKIP_AUTH_SETUP" = 'yes' ]; 
+then
+
+USER={{MONGODB_USERNAME}}
+PASS={{MONGODB_PASSWORD}}
+DB="admin"
+
+# Create User
+echo "Creating user: \"$USER\"..."
+mongo $DB --eval "db.createUser({ user: '$USER', pwd: '$PASS', roles: [ { role: 'userAdminAnyDatabase', db: '$DB' } ] });"
+
+# Stop MongoDB service
+systemctl restart mongod
+
+echo "========================================================================"
+echo "MongoDB User: \"$USER\""
+echo "MongoDB Password: \"$PASS\""
+echo "MongoDB Database: \"$DB\""
+echo "MongoDB Role: \"$ROLE\""
+echo "========================================================================"
+
+rm -f /.db_config
+
+fi
